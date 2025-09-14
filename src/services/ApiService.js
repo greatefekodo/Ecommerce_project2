725dad0679f7f290a9110e9e24603c86c83be50f
greatefekodo <greatefekodo@gmail.com>
2025-09-14 22:27:38 +0100

    Update ApiService to use environment variable

--- src/services/ApiService.js
+++ src/services/ApiService.js
@@ -5,7 +5,7 @@ export default class ApiService {
 
     // static BASE_URL = "http://localhost:8090/api";
 
-    static BASE_URL = "http://13.61.11.219:8090/api";
+    static BASE_URL = process.env.REACT_APP_API_URL;
 
     static saveToken(token) {
         localStorage.setItem("token", token);
